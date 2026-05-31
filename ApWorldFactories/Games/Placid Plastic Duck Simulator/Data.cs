fbae68dea45d146e7ea1a10fd6c147f72575597c
SWCreeperKing <sw.creeperking@gmail.com>
2026-05-30 23:15:29 -0700

    ppds v0.3.0

---
+++ ApWorldFactories/Games/Placid Plastic Duck Simulator/Data.cs
@@ -0,0 +1,68 @@
+﻿namespace ApWorldFactories.Games.Placid_Plastic_Duck_Simulator;
+
+public readonly struct DuckRowData(DataArray param)
+{
+    [Mark] public readonly string DuckName = param;
+    [Mark] public readonly string DuckId = param;
+    [Mark] public readonly int Column = param;
+    [Mark] public readonly DlcType DlcType = param.GetEnum<DlcType>();
+    [Mark] public readonly bool HasUniqueQuack = param;
+    [Mark] public readonly bool SpecialSpawn = param;
+    [Mark] public readonly bool Include = param;
+}
+
+public readonly struct DuckLogicRowData(DataArray param) : ILogicSectorDataType<string, DuckLogicRowData>
+{
+    [Mark] public readonly string DuckName = param;
+    [Mark] public readonly bool IsEventDuck = param;
+    [Mark] public readonly bool IsSeasonalDuck = param;
+    [Mark] public readonly string[] DucksRequired = param;
+    [Mark] public readonly string[] SpecialDuckProperties = param;
+    [Mark] public readonly string[] Map = param;
+
+    public string GetIdentifier() => throw new NotImplementedException();
+    public bool IsMatch(DuckLogicRowData matchAgainst) => throw new NotImplementedException();
+    public bool IsNoOption() => throw new NotImplementedException();
+    public string GenRule() => throw new NotImplementedException();
+    public string GenOption() => "";
+    public string Print() => throw new NotImplementedException();
+}
+
+public readonly struct MapRowData(DataArray param)
+{
+    [Mark] public readonly string MapName = param;
+    [Mark] public readonly string MapId = param;
+}
+
+public readonly struct DlcNameRowData(DataArray param)
+{
+    [Mark] public readonly string DlcName = param.Get(false);
+    public readonly DlcType DlcType = param.GetEnum<DlcType>();
+}
+
+public enum DlcType
+{
+    BaseGame, DucksPlease, QuackingTheIce,
+    DuckAddiction, HippospaceDownload, SoManyDucks,
+    RooftopOnePercent, DucksGalore, Ducklings,
+    VirtualThermae,
+}
+
+public static class DlcTypeHelper
+{
+    public static string OptionName(this DlcType type) => type switch
+    {
+        DlcType.BaseGame => "base_game", DlcType.DucksPlease => "ducks_please",
+        DlcType.QuackingTheIce => "quacking_the_ice", DlcType.DuckAddiction => "duck_addiction",
+        DlcType.HippospaceDownload => "hippospace_download", DlcType.SoManyDucks => "so_many_ducks",
+        DlcType.RooftopOnePercent => "rooftop_one_percent", DlcType.DucksGalore => "ducks_galore",
+        DlcType.Ducklings => "ducklings", DlcType.VirtualThermae => "virtual_thermae", _ => "unknown_dlc"
+    };
+
+    public static string ItemName(this DlcType type) => type switch
+    {
+        DlcType.DucksPlease => "Ducks Please Ducks", DlcType.DuckAddiction => "Duck Addiction Ducks",
+        DlcType.SoManyDucks => "So Many Ducks Ducks", DlcType.DucksGalore => "Ducks Galore Ducks",
+        DlcType.Ducklings => "Ducklings Ducks", _ => ""
+    };
+}
---
+++ ApWorldFactories/Games/Placid Plastic Duck Simulator/PlacidPlasticDuckSim.cs
@@ -0,0 +1,137 @@
+﻿using ApWorldFactories.Games.Slime_Rancher;
+using ApWorldFactories.Graphviz;
+using CreepyUtil.Archipelago.WorldFactory;
+using static ApWorldFactories.PathConstants;
+using static CreepyUtil.Archipelago.WorldFactory.ItemFactory.ItemClassification;
+using static CreepyUtil.Archipelago.WorldFactory.PremadePython;
+
+namespace ApWorldFactories.Games.Placid_Plastic_Duck_Simulator;
+
+public class PlacidPlasticDuckSim : BuildData
+{
+    public override string SteamDirectory => DDrive;
+    public override string ModFolderName => "SW_CreeperKing.Duckipelago";
+    public override string GameName => "Placid Plastic Duck Simulator";
+    public override string ApWorldName => "placidplasticducksim";
+    public override string GoogleSheetId => "1xOW8OJ-Mya3Lgp_vYhXJTQ4B8r41K80tyesA-TwNOXY";
+    public override string WorldVersion => "0.3.0";
+
+    public DuckRowData[] DuckRowData = [];
+    public DuckLogicRowData[] DuckLogicRowData = [];
+    public MapRowData[] MapRowData = [];
+    public Dictionary<DlcType, string> DlcTypeToName = [];
+    public DlcType[] DlcItems = Enum.GetValues<DlcType>().Where(dlc => dlc.ItemName() is not "").ToArray();
+    public int[] Columns = Enumerable.Range(1, 10).ToArray();
+
+    public override void RunShenanigans()
+    {
+        GetSpreadsheet()
+           .ReadTable(out DuckRowData).SkipColumn()
+           .ReadTable(out DuckLogicRowData).SkipColumn()
+           .ReadTable(out MapRowData).SkipColumn()
+           .ReadTable(out DlcNameRowData[] dlcNames);
+
+        DuckRowData = DuckRowData.Where(data => data.Include).ToArray();
+        DlcTypeToName = dlcNames.ToDictionary(data => data.DlcType, data => data.DlcName);
+
+        WriteData(
+            "ducks", DuckRowData.Select(data => $"{data.DuckName}|{data.DuckId}|{(int)data.DlcType}|{data.Column}")
+        );
+    }
+
+    public override void Options(WorldFactory _, OptionsFactory options_fact) =>
+        options_fact
+            // .AddOption(
+            //      "Allow Unpredictable Ducks",
+            //      "Allow ducks that have random, unpredictables spawns to be checks", new Toggle()
+            //  )
+           .AddOption("Ducks Please", "Enable ducks from the Ducks, Please DLC", new Toggle())
+            // .AddOption("Quacking the Ice", "Enable the stage from the Qucking the Ice DLC", new Toggle())
+           .AddOption("Duck Addiction", "Enable ducks from the Duck Addiction DLC", new Toggle())
+            // .AddOption("Hippospace Download", "Enable the stage from the Hippospace Download DLC", new Toggle())
+           .AddOption("So Many Ducks", "Enable ducks from the So Many Ducks DLC", new Toggle())
+            // .AddOption("Rooftop One Percent", "Enable the stage from the Rooftop One Percent DLC", new Toggle())
+           .AddOption("Ducks Galore", "Enable the ducks from the Ducks Galore DLC", new Toggle())
+           .AddOption("Ducklings", "Enable the ducks from the Ducklings DLC", new Toggle())
+            // .AddOption("Virtual Thermae", "Enable the stage from the Virtual Thermae DLC", new Toggle())
+           .AddCheckOptions();
+
+    public override void Locations(WorldFactory _, LocationFactory location_fact)
+    {
+        location_fact.ForEachOf(
+            DuckRowData.GroupBy(data => data.DlcType),
+            (b, group) => b.AddLocations(
+                group.Key.OptionName(), group.Select(data => (string[])[data.DuckName, $"Column {data.Column}"])
+            )
+        );
+    }
+
+    public override void Items(WorldFactory _, ItemFactory item_fact)
+    {
+        item_fact.AddItem("Progressive Column Unlock", Progression)
+                 .AddItem("Progressive Spawn Speed Upgrade", Useful)
+                 .AddItem("Random Duck", Filler)
+                 .AddItemListVariable("dlc_items", Progression, list: DlcItems.Select(dlc => dlc.ItemName()).ToArray())
+                 .AddCreateItems(method =>
+                      method.AddCode(CreateItemsFromCountGenCode("9", "Progressive Column Unlock"))
+                            .AddCode(CreateItemsFromCountGenCode("9", "Progressive Spawn Speed Upgrade"))
+                            .ForEachOf(
+                                 DlcItems, (b, dlc) => b.AddCode(
+                                     new IfFactory($"options.{dlc.OptionName()}")
+                                        .AddCode(CreateItem(dlc.ItemName()))
+                                 )
+                             )
+                            .AddCode(CreateItemsFillRemainingWithItem("Random Duck"))
+                  );
+    }
+
+    public override void Rules(WorldFactory _, RuleFactory rule_fact) 
+        => rule_fact.AddCompoundLogicFunction(
+        "col", "has_column", "hasN[\"Progressive Column Unlock\", number - 1]", "number"
+    )
+   .AddLogicRules(
+        DuckRowData.Where(data => data.DlcType is not DlcType.BaseGame).ToDictionary(data => data.DuckName, data => $"has[\"{data.DlcType.ItemName()}\"]")
+        );
+
+    public override void Regions(WorldFactory _, RegionFactory region_fact)
+    {
+        region_fact.AddRegions(regions: Columns.Select(i => $"Column {i}").ToArray())
+                   .ForEachOf(
+                        Columns,
+                        (b, i) => b.AddConnectionCompiledRule(
+                            i == 1 ? "Menu" : $"Column {i - 1}", $"Column {i}", $"col[{i}]"
+                        )
+                    )
+                   .AddLocationsFromList("base_game")
+                   .ForEachOf(
+                        DlcItems,
+                        (b, dlc) => b.AddLocationsFromList(dlc.OptionName(), condition: $"options.{dlc.OptionName()}")
+                    );
+    }
+
+    public override void Init(WorldFactory world_fact, WorldInitFactory init_fact)
+    {
+        init_fact
+           .UseItemGroups(new Dictionary<string, string> { ["column"] = "[\"Progressive Column Unlock\"]" })
+           .UseInitFunction()
+           .AddUseUniversalTrackerPassthrough(yamlNeeded: false)
+           .UseCreateRegions()
+           .AddCreateItems()
+           .UseSetRules(method => method.AddCode(CreateGoalCondition("col[10]", world_fact.GetRuleFactory())))
+           .UseFillSlotData()
+           .InjectCodeIntoWorld(world => world.AddVariable(new Variable("gen_puml", "False")))
+           .UseGenerateOutput(method => method.AddCode(PumlGenCode()));
+    }
+
+    public override string GenerateGraphViz(WorldFactory worldFactory, Dictionary<string, string> associations,
+        Func<string, string> getRule,
+        string[][] locationDoubleArrays)
+    {
+        return new GraphBuilder(GameName)
+              .ForEachOf(
+                   Columns, (b, i) => b.AddConnection(i == 1 ? "Menu" : $"Column {i - 1}", $"Column {i}", $"col[{i}]")
+               )
+              .AddLocationsFromDoubleArray(locationDoubleArrays, getRule)
+              .GenString();
+    }
+}
